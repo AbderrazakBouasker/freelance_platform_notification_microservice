18530484f3942a3fd131853a3563b6a7bc7dd314
Jose Mokeni <jmmokeni@gmail.com>
2024-10-14 19:46:04 +0100

    Added Dockerfile and updated properties

---
+++ src/main/java/com/microservice/notification/controller/Hello.java
@@ -0,0 +1,14 @@
+package com.microservice.notification.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Hello {
+
+    @GetMapping("/notifications/hello")
+    String sayHello() {
+        return "Hello World";
+    }
+
+}
